abf85c7098e93f2512aa7b8505c1986c956ee140
Alexander <alejandrokennedy@gmail.com>
2025-04-24 16:24:45 -0700

    further env var debugging

--- apps/web/scripts/print-env.js
+++ apps/web/scripts/print-env.js
@@ -1,3 +1,4 @@
 // console.log("DATABASE_URL:", process.env.DATABASE_URL);
-console.log("DATABASE_URL:", process.env.DATABASE_URL ? process.env.DATABASE_URL.slice(0, 20) + "..." : "Database URL is undefined");
-console.log("DEBUG_DATABASE_URL:", process.env.DEBUG_DATABASE_URL)
+// console.log("DATABASE_URL:", process.env.DATABASE_URL ? process.env.DATABASE_URL.slice(0, 20) + "..." : "Database URL is undefined");
+// console.log("DEBUG_DATABASE_URL:", process.env.DEBUG_DATABASE_URL)
+console.log("ALL ENV VARS:", JSON.stringify(process.env, null, 2));
