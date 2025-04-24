fff06ce91dbf328232eccb30d141657be2738e45
Alexander <alejandrokennedy@gmail.com>
2025-04-24 16:02:00 -0700

    adding debug script for DEBUG_DATABASE_URL

--- apps/web/scripts/print-env.js
+++ apps/web/scripts/print-env.js
@@ -1,2 +1,3 @@
 // console.log("DATABASE_URL:", process.env.DATABASE_URL);
 console.log("DATABASE_URL:", process.env.DATABASE_URL ? process.env.DATABASE_URL.slice(0, 20) + "..." : "Database URL is undefined");
+console.log("DEBUG_DATABASE_URL:", process.env.DEBUG_DATABASE_URL)
