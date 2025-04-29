525a59dfd18cf23a0b744be2f5a6b9c862db7662
Alexander <alejandrokennedy@gmail.com>
2025-04-29 15:43:28 -0700

    updated code in db.ts to provide explicit type for createPrismaClient

--- apps/web/src/server/db.ts
+++ apps/web/src/server/db.ts
@@ -2,15 +2,26 @@ import { PrismaClient } from "@prisma/client";
 
 import { env } from "~/env";
 
-const createPrismaClient = () =>
+const createPrismaClient = (): PrismaClient => 
   new PrismaClient({
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
-};
+  const globalForPrisma = globalThis as unknown as {
+    prisma: PrismaClient | undefined;
+  };
+  
+// // OLD CODE
+// const createPrismaClient = () =>
+//   new PrismaClient({
+//     log:
+//       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+//   });
+
+// const globalForPrisma = globalThis as unknown as {
+//   prisma: ReturnType<typeof createPrismaClient> | undefined;
+// };
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
 
